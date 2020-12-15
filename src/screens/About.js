00c3120f0f3fff4993d07c6b00e4b588ff72cdc2
JChris246 <chris.401@live.com>
2020-12-15 14:50:07 -0400

    Added about page

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,7 +1,20 @@
+import profile_pic from "../assets/IMG-20190902-WA0003.jpg"
+
 const About = () => {
 
     return (
-        <h1 className="text-blue-400">About</h1>
+        <div className="w-5/6 mx-auto">
+            <div className="mt-14 mx-4 flex flex-row">
+                <img src={profile_pic} className="rounded-full" alt="pic of me" height="12"></img>
+                <div className="ml-8 mt-8">
+                    <span className="text-2xl">Hey,</span>
+                    <p></p>
+                    <br/><span className="text-lg md:text-3xl md:ml-14">My name is Christopher King</span>
+                    <p class="mt-6"></p><a className="md:hidden text-blue-500" href="https://github.com/JChris246">My githhub</a>
+                </div>
+            </div>
+            <span className="mt-12 md:w-5/6 inline-block">I am a third-year student at The University of the West Indies, Cave Hill, Studying Computer Science. My objective is to have a career in computer programming. I am very passionate about working with computers and I am adaptable and open to new ideas and concepts.</span>
+        </div>
     )
 }
 
